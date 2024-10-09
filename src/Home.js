0caec57ee696edab3137ffab2e382105b670093f
AlanBitts <rui-viveiros@hotmail.com>
2024-10-09 12:03:46 +0100

    added some props

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'O meu novo website', body: 'lorem ipsum...', author:'Rui', id: 1},
+        { title: 'Festa de lançamento!', body: 'lorem ipsum...', author:'Marie', id: 2},
+        { title: 'Dicas de programaçao web', body: 'lorem ipsum...', author:'Rui', id: 3}
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+           <BlogList blogs={blogs.filter((blog) => blog.author === "Rui")}  title="As minhas publicaçōes" />
+           <BlogList blogs={blogs.filter((blog) => blog.author != "Rui")}  title="Publicaçōes de amigos" />
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Rui Viveiros | Blog</h1>
             <div className="links">
-                <a href="/about">Sobre mim</a>
+                <a href="/about">Sobre</a>
                 <a href="/projects">Projetos</a>
                 <a href="/create">New Post</a>
             </div>
