1d48c40c074441ce521bdc594ef21deab88e371a
AlanBitts <rui-viveiros@hotmail.com>
2024-10-09 10:10:12 +0100

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Rui Viveiros | Blog</h1>
+            <div className="links">
+                <a href="/about">Sobre mim</a>
+                <a href="/projects">Projetos</a>
+                <a href="/create">New Post</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
